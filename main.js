f30502488af7bc99cb2b8403ede68521da3bd63b
samanthasheadavis <samantha.shea.davis@gmail.com>
2016-09-27 19:13:40 -0400

    final version

---
+++ main.js
@@ -0,0 +1,34 @@
+function wordCount(string) {
+    //1. Split string into it's components
+    var stringtoSplit = string;
+    var splitString = string.split(" ");
+    var words = {};
+    //2. omit any single character words
+
+    //a. count characters in each word and log index for splicing
+    var count = 0;
+    while (count < splitString.length) {
+
+        //b. if word has 1 character, splice out
+        if (splitString[count].length === 1) {
+            splitString.splice(splitString.indexOf(splitString[count]), splitString[count].length);
+        }
+        count++;
+    }
+    // 3. Determine frequency of words in array and update them in object
+
+    for (count = 0; count < splitString.length; count++) {
+        var currentWord = splitString[count];
+          if (currentWord in words){
+            words[currentWord] ++;
+          } else {
+            words[currentWord] = 1;
+          }
+    }
+
+    console.log(words);
+
+
+}
+
+wordCount("Music to hear, why hear'st thou music sadly? Sweets with sweets war not, joy delights in joy: Why lov'st thou that which thou receiv'st not gladly, Or else receiv'st with pleasure thine annoy? If the true concord of well-tuned sounds, By unions married do offend thine ear, They do but sweetly chide thee, who confounds In singleness the parts that thou shouldst bear: Mark how one string sweet husband to another, Strikes each in each by mutual ordering; Resembling sire, and child, and happy mother, Who all in one, one pleasing note do sing: Whose speechless song being many, seeming one, Sings this to thee, 'Thou single wilt prove none'.");
